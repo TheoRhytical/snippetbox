e1c9448324906b393c6ed5896972f42554580b8e
TheoRhytical <rhyscabonita@gmail.com>
2024-06-24 00:45:01 +0800

    e2e tests helper functions
    - Added helper functions for e2e testing
    - Cleaned up GET /ping e2e test

--- cmd/snippetbox/handlers_test.go
+++ cmd/snippetbox/handlers_test.go
@@ -3,7 +3,6 @@ package main
 import (
 	"bytes"
 	"io"
-	"log"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -12,29 +11,14 @@ import (
 )
 
 func TestPing(t *testing.T) {
-	app := &application{
-		errorLog: log.New(io.Discard, "", 0),
-		infoLog:  log.New(io.Discard, "", 0),
-	}
-
-	ts := httptest.NewTLSServer(app.routes())
+	app := newTestApplication(t)
+	ts := newTestServer(t, app.routes())
 	defer ts.Close()
 
-	rs, err := ts.Client().Get(ts.URL + "/ping")
-	if err != nil {
-		t.Fatal(err)
-	}
+	statusCode, _, body := ts.get(t, "/ping")
 
-	assert.Equal(t, rs.StatusCode, http.StatusOK)
-
-	defer rs.Body.Close()
-	body, err := io.ReadAll(rs.Body)
-	if err != nil {
-		t.Fatal(err)
-	}
-	bytes.TrimSpace(body)
-
-	assert.Equal(t, string(body), "OK")
+	assert.Equal(t, statusCode, http.StatusOK)
+	assert.Equal(t, body, "OK")
 }
 
 func TestPingUnit(t *testing.T) {
